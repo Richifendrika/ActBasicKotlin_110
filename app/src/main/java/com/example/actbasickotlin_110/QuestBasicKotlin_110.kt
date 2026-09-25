34b622369c34de6afbd3c9943f944549bbfdfaa0
Richifendrika <Richi.fendrika.ft21@mail.umy.ac.id>
2026-09-25 19:26:07 +0700

    menambahkan list

--- app/src/main/java/com/example/actbasickotlin_110/QuestBasicKotlin_110.kt
+++ app/src/main/java/com/example/actbasickotlin_110/QuestBasicKotlin_110.kt
@@ -3,15 +3,31 @@ package com.example.actbasickotlin_110
 //fun main() {
 //    println("Hello, world!")
 //}
+
+//fun main() {
+//
+//    val nama = "Budi"
+//    var umur = 20
+//
+//    println(nama)
+//    println(umur)
+//
+//    umur = 21
+//
+//    println(umur)
+//}
+
 fun main() {
+    // Read only list
+    val readOnlyShapes = listOf("triangle", "square", "circle")
+    println(readOnlyShapes)
+    // [triangle, square, circle]
 
-    val nama = "Budi"
-    var umur = 20
+    // Mutable list with explicit type declaration
+    val shapes: MutableList<String> = mutableListOf("triangle", "square", "circle")
+    println(shapes)
+    // [triangle, square, circle]
 
-    println(nama)
-    println(umur)
-
-    umur = 21
-
-    println(umur)
+    // Read-Only view dari mutable list
+    val shapesLocked: List<String> = shapes
 }
