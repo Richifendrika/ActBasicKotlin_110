96562436b0d9b21f789d2f058351b7ea8d0cf24f
Richifendrika <Richi.fendrika.ft21@mail.umy.ac.id>
2026-09-25 19:04:51 +0700

    menambahkan variabel nama&umur

--- app/src/main/java/com/example/actbasickotlin_110/QuestBasicKotlin_110.kt
+++ app/src/main/java/com/example/actbasickotlin_110/QuestBasicKotlin_110.kt
@@ -1,5 +1,17 @@
 package com.example.actbasickotlin_110
 
+//fun main() {
+//    println("Hello, world!")
+//}
 fun main() {
-    println("Hello, world!")
+
+    val nama = "Budi"
+    var umur = 20
+
+    println(nama)
+    println(umur)
+
+    umur = 21
+
+    println(umur)
 }
